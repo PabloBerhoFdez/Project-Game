ee5553c378068e7915b002956a0593e3fafa8024
PabloBerho <pablo_fernandez_12@hotmail.com>
2020-11-01 00:28:33 +0100

    added new functionalities

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 window.onload = () => {
+
+  //-----------------------VARIABLES--------------------//
+
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
+  class Villains {
+    constructor(_x, _y, _width, _height) {
+      this.x = _x
+      this.y = _y
+      this.width = _width
+      this.height = _height
+    }
+  }
+  let villainArr = []
+  let direction = 'up'
+  let gravity = 0.1
+  let hero = {
+    x: 500,
+    y: 390,
+    vx: 0,
+    vy: 2
+  }
+  // let newVillain = new Villains (100, 390, 100, 100)
+
 
 	//----------------FUNCIONES---------------------//
 	function createStage(){
@@ -24,15 +46,55 @@ window.onload = () => {
 		villainImg.onload = () => {
 			ctx.drawImage(villainImg, 100, 390, 100, 100)
 		}
-		villainImg.src = './images/nomu.png'
-	}
+    villainImg.src = './images/nomu.png'
+    moveVillain()
+  }
 
-	function updateCanvas() {
+	function generateVillain() {
+    setInterval(() => {
+      console.log(villainArr)
+      villainArr.push(new Villains(100, 390, 100, 100))
+    }, 2000)
+  }
+
+  function moveVillain () {
+    villainArr.forEach(villain => {
+      return villain.x -= 2
+    });
+  }
+
+  function jump(){
+    if (direction === 'up'){
+      hero.x += 10
+      createHero()
+    }
+  }
+  function gravityHero(){
+    // ctx.clearRect (0, 0, 700, 500)
+    hero.draw()
+    hero.vy += gravity
+    hero.x += hero.vx;
+    hero.y += hero.vy;
+  }
+
+  document.addEventListener('keydown', event =>{
+    if(event.key === 'ArrowUp'){
+      console.log('Holi guapi')
+      createHero()
+      direction = 'up'
+      jump()
+    }
+  })
+
+  function gameStage() {
 		createStage()
 		createHero()
-		createVillain()
-	}
+    createVillain()
 
-	updateCanvas()
+    requestAnimationFrame(gameStage)
+  }
+  // generateVillain()
+
+  gameStage()
 
 }
