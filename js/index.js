c1cd4ee3dbe16657216bb3fda82bdb873afdd040
PabloBerho <pablo_fernandez_12@hotmail.com>
2020-11-01 20:12:10 +0100

    Gravity added + villain movement

--- js/index.js
+++ js/index.js
@@ -4,24 +4,20 @@ window.onload = () => {
 
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
-  class Villains {
-    constructor(_x, _y, _width, _height) {
+  class Character {
+    constructor(_x, _y, _width, _height, _vx, _vy) {
       this.x = _x
       this.y = _y
       this.width = _width
       this.height = _height
+      this.vx = _vx
+      this.vy = _vy
     }
   }
   let villainArr = []
-  let direction = 'up'
-  let gravity = 0.1
-  let hero = {
-    x: 500,
-    y: 390,
-    vx: 0,
-    vy: 2
-  }
-  // let newVillain = new Villains (100, 390, 100, 100)
+  let newHero = new Character (100, 390, 100, 100, 0, 2)
+  let gravityDown = 0.0005
+  let gravityUp = 1
 
 
 	//----------------FUNCIONES---------------------//
@@ -36,64 +32,83 @@ window.onload = () => {
 	function createHero() {
 		const heroImg = new Image ()
 		heroImg.onload = () => {
-			ctx.drawImage(heroImg, 500, 390, 100, 100)
+			ctx.drawImage(heroImg, 500, newHero.y, 100, 100)
 		}
 		heroImg.src = './images/midoriya.png'
-	}
+  }
+
+  function moveHero() {
+    newHero.y -= 220
+  }
+
+  function heroGravityDown() {
+    newHero.vy += gravityDown
+    newHero.y += newHero.vy
+  }
+
+  function heroGravityUp() {
+    newHero.vy -= gravityUp
+    newHero.y -= newHero.vy
+  }
+
+  function stopHero() {
+    let heroBottom = 390
+    let heroUp = 170
+    if (newHero.y > heroBottom) {
+      newHero.y = heroBottom
+      clearInterval(heroBottom)
+    }
+    if (newHero.y < heroUp){
+      newHero.y = heroUp
+      clearInterval(heroUp)
+    }
+  }
+  
+  document.addEventListener('keydown', event =>{
+    if(event.key === 'ArrowUp'){
+      createHero()
+      moveHero()
+    }
+  })
 
 	function createVillain() {
-		const villainImg = new Image ()
-		villainImg.onload = () => {
-			ctx.drawImage(villainImg, 100, 390, 100, 100)
-		}
-    villainImg.src = './images/nomu.png'
+    villainArr.forEach(villain => {
+      const villainImg = new Image ()
+		  villainImg.onload = () => {
+			ctx.drawImage(villainImg, villain.x, 390, 100, 100)
+		  }
+      villainImg.src = './images/nomu.png'
+    });
+		
     moveVillain()
   }
 
 	function generateVillain() {
     setInterval(() => {
       console.log(villainArr)
-      villainArr.push(new Villains(100, 390, 100, 100))
+      villainArr.push(new Character(-100, 390, 100, 100))
     }, 2000)
   }
 
   function moveVillain () {
     villainArr.forEach(villain => {
-      return villain.x -= 2
+      return villain.x += 3
     });
   }
 
-  function jump(){
-    if (direction === 'up'){
-      hero.x += 10
-      createHero()
-    }
-  }
-  function gravityHero(){
-    // ctx.clearRect (0, 0, 700, 500)
-    hero.draw()
-    hero.vy += gravity
-    hero.x += hero.vx;
-    hero.y += hero.vy;
-  }
-
-  document.addEventListener('keydown', event =>{
-    if(event.key === 'ArrowUp'){
-      console.log('Holi guapi')
-      createHero()
-      direction = 'up'
-      jump()
-    }
-  })
 
   function gameStage() {
 		createStage()
 		createHero()
     createVillain()
+    heroGravityDown()
+    heroGravityUp()
+    stopHero()
 
     requestAnimationFrame(gameStage)
   }
-  // generateVillain()
+
+  generateVillain()
 
   gameStage()
 
